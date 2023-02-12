7558f157da2f6b9d5047d87e80611e0c667d2dfb
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-02-12 18:53:42 +0500

    feat: draw status

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function Board({ xIsNext, squares, onPlay }) {
     let status;
     if (winner) {
         status = "Winner: " + winner;
+    } else if (!squares.includes(null)) {
+        status = "Draw";
     } else {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
