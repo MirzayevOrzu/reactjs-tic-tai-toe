333f6f66bc160c68bbcdd377126bcc0ff22e6946
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-02-12 18:31:55 +0500

    feat: build board with loop istead of hard coding

--- src/App.js
+++ src/App.js
@@ -33,48 +33,21 @@ function Board({ xIsNext, squares, onPlay }) {
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square
-                    value={squares[0]}
-                    onSquareClick={() => handleClick(0)}
-                />
-                <Square
-                    value={squares[1]}
-                    onSquareClick={() => handleClick(1)}
-                />
-                <Square
-                    value={squares[2]}
-                    onSquareClick={() => handleClick(2)}
-                />
-            </div>
-            <div className="board-row">
-                <Square
-                    value={squares[3]}
-                    onSquareClick={() => handleClick(3)}
-                />
-                <Square
-                    value={squares[4]}
-                    onSquareClick={() => handleClick(4)}
-                />
-                <Square
-                    value={squares[5]}
-                    onSquareClick={() => handleClick(5)}
-                />
-            </div>
-            <div className="board-row">
-                <Square
-                    value={squares[6]}
-                    onSquareClick={() => handleClick(6)}
-                />
-                <Square
-                    value={squares[7]}
-                    onSquareClick={() => handleClick(7)}
-                />
-                <Square
-                    value={squares[8]}
-                    onSquareClick={() => handleClick(8)}
-                />
-            </div>
+            {Array(3)
+                .fill("")
+                .map((_, i) => (
+                    <div className="board-row" key={i}>
+                        {Array(3)
+                            .fill("")
+                            .map((_, j) => (
+                                <Square
+                                    key={j}
+                                    value={squares[i * 3 + j]}
+                                    onSquareClick={() => handleClick(i * 3 + j)}
+                                />
+                            ))}
+                    </div>
+                ))}
         </>
     );
 }
